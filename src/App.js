e6e1e104816a8d8bcc28accf93750aa4f845e397
xgao35 <114840383+hungrylizard232@users.noreply.github.com>
2022-11-22 00:48:09 -0500

    layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
 import logo from './logo.svg';
 import './App.css';
+import duckData from "./assets/duckData.json";
+import DuckItem from './DuckItem';
+
+import { useState } from "react";
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [cart, setCart] =useState({});
+  const [price, setPrice] = useState(0);
+  const increa = (uid) => {
+    let newCart = cart;
+    if (newCart[uid]) {
+      newCart[uid] += 1;
+    } else {
+      newCart[uid] = 1;
+    }
+    
+    setPrice(price + duckData[uid].price)
+    setCart({...newCart});
+  }
+  const decreaseCart = (uid) => {
+    let newCart = cart;
+    if (newCart[uid]) {
+      newCart[uid] -= 1;
+      setPrice(price - duckData[uid].price);
+    }
+    setCart({...newCart});
+    
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">  
+        <div>
+          <h1>Rubber Duck store</h1>
+          <div className='duck-flexholder'>
+          {duckData.map((item, index) => {
+          return (
+          <DuckItem increaseCart={increa} decreaseCart={decreaseCart} item = {item} index = {index} />
+          )          
+          })}
+          </div>
+        </div>
+
+          <div className='checkout'>
+            <h2>Cart</h2>
+            <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+            <Card.Body>
+              <Card.Title>My Cart</Card.Title>
+              <Card.Text>
+              
+            total: ${price}
+              </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+            {Object.keys(cart).map((key) => {
+              return (
+                <ListGroup.Item>{duckData[key].name + ": " + cart[key]}</ListGroup.Item>
+              )
+              
+            })}
+            </ListGroup>
+            <Card.Body>
+              <Card.Link href="#">Card Link</Card.Link>
+              <Card.Link href="#">Another Link</Card.Link>
+            </Card.Body>
+          </Card>
+          </div>
+
+          
+
+    
+      
     </div>
+
+    
   );
 }
 
---
+++ src/DuckItem.js
@@ -0,0 +1,22 @@
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+export default function DuckItem(props) {
+    return (
+        <div className='duck-container'>
+            <Card style={{ width: '80%' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+                <Card.Title>{props.item.name}</Card.Title>
+                <Card.Text>
+                ${props.item.price}
+                </Card.Text>
+                <Button variant="secondary" onClick = {() => props.increaseCart(props.index)}>+</Button>
+                <Button variant="secondary" onClick = {() => props.decreaseCart(props.index)}>-</Button>
+            </Card.Body>
+            </Card>
+            
+        </div>
+      
+    )
+  }
